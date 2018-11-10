1eb3a1ada0db59a3ffeb7544968733f2a560fa3a
Raphael Silveira <silveira@angellira.com.br>
2018-11-10 17:27:09 -0200

    criação do projeto

---
+++ ApiCalcTeste/Controllers/CalculaJurosController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiCalcTeste.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    public class CalculaJurosController : Controller
+    {
+
+        // GET api/calculajuros
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+    }
+}
---
+++ ApiCalcTeste/Controllers/ShowMeTheCodeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiCalcTeste.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    public class ShowMeTheCodeController : Controller
+    {
+        // GET api/showmethecode
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(Environment.GetEnvironmentVariable("URL_CODE"));
+        }
+    }
+}
